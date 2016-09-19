76d1d670d824def21936462955e39036d214d883
JoshuaCurry <josh.curry@talktalk.net>
2016-09-19 23:26:00 +0100

    Added more

---
+++ johncena.sh
@@ -0,0 +1,15 @@
+beep -f  659.25 -l 375
+beep -f 1174.66 -l 375 
+beep -f 1318.51 -l 188 
+beep -f 1046.50 -l 377 
+beep -f 1174.66 -l 1500 
+beep -f 19999 -l 375
+
+beep -f  659.25 -l 375
+beep -f 1396.91 -l 375
+beep -f 1318.51 -l 188
+beep -f 1046.50 -l 377
+beep -f 1174.66 -l 1500
+
+#Author: mitchhentges
+#https://github.com/mitchhentges/john-cena-theme-linux-beep/blob/master/startupsong.sh
